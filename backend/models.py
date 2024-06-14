f2d01622aa59c82b4336ef8df006c4681794e2ab
Smit Shah <138520584+shahsmit293@users.noreply.github.com>
2024-06-14 23:50:31 +0000

    added entrypoint.sh file

--- backend/models.py
+++ backend/models.py
@@ -15,3 +15,17 @@ class User(db.Model):
             "id": self.id,
             "name": self.name
         }
+
+class address(db.Model):
+    __tablename__ = 'address'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, address):
+        self.address = address
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "address": self.address
+        }
